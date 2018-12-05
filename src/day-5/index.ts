1f07e46678c20362901c93b09f05b915506f8b0e
Ishan Vadwala <ishan.vadwala@veracity.vision>
2018-12-05 14:19:28 -0600

    clean up day 5

--- src/day-5/index.ts
+++ src/day-5/index.ts
@@ -1,37 +1,30 @@
-import * as _ from 'lodash';
 import * as fs from 'fs';
 import * as path from 'path';
 
 function solveA(str: string) {
   const result: string[] = [];
-  let start = 0;
-  while (str.charAt(start)) {
+  for (let start = 0; start < str.length; start++) {
     const prev = result.pop();
     const curr = str.charAt(start);
-
-    if (prev !== curr && (prev || '').toLowerCase() === curr.toLowerCase()) {
-      start++;
-      continue;
-    } else {
-      if (prev) result.push(prev);
+    if (!prev) {
       result.push(curr);
-      start++;
+      continue;
     }
+    
+    if (prev !== curr && prev.toLowerCase() === curr.toLowerCase()) continue;
+    result.push(prev);
+    result.push(curr);
   }
 
-  return result.join('').length;
+  return result.length;
 }
 
 function solveB(str: string) {
   const alphabets = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  let lowestCount = 9999999999;
+  let lowestCount = Number.POSITIVE_INFINITY;
   for (const alphabet of alphabets) {
-    console.log(alphabet);
-    let regex = new RegExp(alphabet.toUpperCase(), 'g');
+    let regex = new RegExp(alphabet, 'ig');
     let localStr = str.replace(regex, '');
-
-    regex = new RegExp(alphabet.toLowerCase(), 'g');
-    localStr = localStr.replace(regex, '');
 
     const charCount = solveA(localStr);
     if (charCount < lowestCount) {
