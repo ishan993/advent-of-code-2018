883acb89d6d74dba9ea126419436ac9705b9a671
Ishan Vadwala <ishan.vadwala@veracity.vision>
2018-12-09 20:47:22 -0600

    day8

---
+++ src/day-8/index.ts
@@ -0,0 +1,63 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+function solveA(inputs: number[]) {
+  const childNodesCount = inputs.shift() || 0;
+  const metadataCount = inputs.shift() || 0;
+
+  let childSum = 0;
+
+  for (let i = 0; i < childNodesCount; i++) {
+    childSum += solveA(inputs);
+  }
+
+  let rootCount = 0;
+  for (let i = 0; i < metadataCount; i++) {
+    rootCount += inputs.shift() || 0;
+  }
+
+  return childSum + rootCount;
+}
+
+function solveB(inputs: number[], level: number = 0) {
+  const childNodesCount = inputs.shift() || 0;
+  const metadataCount = inputs.shift() || 0;
+  const childResultMap: Record<string, number> = {};
+  let childSum = 0;
+
+  for (let i = 0; i < childNodesCount; i++) {
+    const thisChildSum = solveB(inputs, level + 1);
+    childResultMap[i] = thisChildSum;
+    childSum += thisChildSum;
+  }
+
+  let rootSum = 0;
+  if (!childNodesCount) {
+    for (let i = 0; i < metadataCount; i++) {
+      const metadatum = inputs.shift() || 0;
+      rootSum += metadatum;
+    }
+
+    rootSum += childSum;
+  } else {
+    for (let i = 0; i < metadataCount; i++) {
+      const metadatum = inputs.shift() || 0;
+      rootSum += childResultMap[metadatum - 1] || 0;
+    }
+  }
+
+  return rootSum;
+}
+
+function main() {
+  const filePath = path.join(__dirname, 'input.txt');
+  const inputs = fs.readFileSync(filePath, 'utf8')
+    .split(' ')
+    .filter(r => r !== '' && r !== ' ')
+    .map(r => Number(r));
+
+  console.log('result1: ', solveA(inputs.slice()));
+  console.log('result2: ', solveB(inputs.slice()));
+}
+
+main();
